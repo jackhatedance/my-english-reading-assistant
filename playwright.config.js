3bc0a70781cbf0f96984bd84dbf3315aa635c5b6
jackhatedance <jackhatedance@gmail.com>
2025-05-29 21:48:22 +0800

    set e2e test timeout to 60s

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  timeout: 60 * 1000,
   testDir: './tests/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
