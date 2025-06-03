941ff3ef643fe6590e2719c4e4c913a9085cb913
jackhatedance <jackhatedance@gmail.com>
2025-06-03 09:25:53 +0800

    text definition support phrase

--- src/dictionary/text/TextDefinitionParser.js
+++ src/dictionary/text/TextDefinitionParser.js
@@ -14,7 +14,9 @@ class TextDefinitionParser extends DefinitionParser {
             return [];
         }
     
-        let entry = this.parseEntry(rawDefinition);    
+        let entry = this.parseEntry(rawDefinition);
+        this.afterParseEntry(entry);
+
         let entries = [entry];
         this.afterParse(entries);
         return entries;
@@ -22,6 +24,17 @@ class TextDefinitionParser extends DefinitionParser {
 
     afterParse(entries){
         
+    }
+
+    afterParseEntry(entry){
+        let groups = entry.definitionGroups;
+        let group = groups.find(item => item.name == 'phr.');
+        if(group){
+            let index = groups.indexOf(group);
+            groups.slice(index, 1);
+        
+            entry.phrases = group.definitions.map(item => item.text);
+        }
     }
 
     parseEntry(text){
