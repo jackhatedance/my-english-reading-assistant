2748be8da7950727ef431e743b8b6a25de64dd76
jackhatedance <jackhatedance@gmail.com>
2025-09-11 10:09:55 +0800

    change quick actions to buttons

--- src/components/popup/Header.vue
+++ src/components/popup/Header.vue
@@ -8,6 +8,13 @@ const props = defineProps({
 
 const t = chrome.i18n.getMessage;
 
+function onClickEpub(){
+    chrome.tabs.create({url: 'https://app.flowoss.com/'});
+}
+
+function onClickPdf(){
+    chrome.tabs.create({url: 'https://mozilla.github.io/pdf.js/web/viewer.html'});
+}
 
 function onClickDictionary(){
     chrome.tabs.create({url: chrome.runtime.getURL('dictionary.html')});
@@ -41,6 +48,11 @@ init();
 
 <template>
     <div id="options-container">
+        
+        <a href="#" :title="t('popupEpub')"><img src="icons/epub.png" alt="Epub Reader" @click="onClickEpub"></a>
+        
+        <a href="#" :title="t('popupPdf')"><img src="icons/pdf.png" alt="PDF Reader" @click="onClickPdf"></a>
+
         <a id="dictionary" href="#" :title="t('popupDictionary')"><img src="icons/dictionary.png" alt="dictionary" @click="onClickDictionary"></a>
         
         <a v-show="props.options.report.enabled" id="report" href="#" :title="t('popupReport')"><img src="icons/report.png" alt="report" @click="onClickReport"></a>
--- src/components/popup/NothingToDo.vue
+++ src/components/popup/NothingToDo.vue
@@ -16,13 +16,6 @@ init();
         <br>
         <h1>{{ t('popupNothingToDoTitle') }}</h1>
         <img src="icons/nothing-to-do.png" width="277px">
-        <div class="quick-actions">
-          <h1 class="title">{{ t('popupNothingToDoQuickActionsTitle') }}</h1>
-          <ul class="actions">
-            <li><a href="https://app.flowoss.com" target="_blank">{{ t('popupNothingToDoQuickActionsEpubReader') }}</a></li>
-            <li><a href="https://mozilla.github.io/pdf.js/web/viewer.html" target="_blank">{{ t('popupNothingToDoQuickActionsPdfReader') }}</a></li>            
-          </ul>
-        </div>
       </div>
 </template>
 <style>
