6d19649a02cedf1872a2a0e1631ace5935978542
jackhatedance <jackhatedance@gmail.com>
2025-12-11 10:57:28 +0800

    add log prefix

--- src/log.js
+++ src/log.js
@@ -1,11 +1,25 @@
 import log from 'loglevel'
+import prefix from 'loglevel-plugin-prefix';
+
+// Define the format and content of your prefix
+prefix.reg(log);
+prefix.apply(log, {
+  template: '[%t] %l:', // %t for timestamp, %l for log level
+  timestampFormatter: function (date) {
+    // Customize the timestamp format
+    return date.toLocaleTimeString(); // e.g., '2025-12-10T18:49:00.000Z'
+  },
+  levelFormatter: function (level) {
+    return level.toUpperCase(); // e.g., 'INFO', 'WARN'
+  },
+});
 
 function initLog(){
     log.setDefaultLevel('info');
     log.getLogger("background").setLevel("debug");
     log.getLogger("mera").setLevel("warn");
     //article is about tokenize text
-    log.getLogger("article").setLevel("debug");
+    log.getLogger("article").setLevel("info");
     log.getLogger("page").setLevel("info");
     log.getLogger("page-change-monitor").setLevel("info");
     log.getLogger("mutation-observer").setLevel("info");
