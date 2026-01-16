f7e9a639aca9878ffb6db2cc6f71dfaacb616007
jackhatedance <jackhatedance@gmail.com>
2026-01-16 11:32:14 +0800

    fix element-plus component e2e issue

--- tests/e2e/element-plus/select.cjs
+++ tests/e2e/element-plus/select.cjs
@@ -13,11 +13,11 @@ export class Select {
         const inputLocator = this.root.locator('.el-select__input');
         const ariaControls = await inputLocator.getAttribute('aria-controls');
 
-        let controlsId = ariaControls.split('-')[2];
+        //let controlsId = ariaControls.split('-')[2];
         
-        let optionsId = 'el-popper-container-' + controlsId;
+        //let optionsId = 'el-popper-container-' + controlsId;
         //console.log(optionsId);
-        const optionsLocator = this.root.page().locator('#'+ optionsId);
+        const optionsLocator = this.root.page().locator('#'+ ariaControls);
         const selectedTextElement = optionsLocator.locator(`.el-select-dropdown__item>span:has-text("${text}")`);
         await selectedTextElement.click();
     }
